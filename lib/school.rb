5f26808c1a8df0fb0020655403a5865efa3d0809
Elizabeth Cobb <cobbeec@gmail.com>
2020-09-06 20:13:04 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,10 +12,11 @@ def add_student (student, level)
   roster [level]. push student 
 end 
 
-def grade (level)
-  roster.detect do |x,y|
-    if x == level
-      return y 
-    end 
+def sort 
+  nu_hash = {}
+  roster.each do |x,y|
+  nu_hash[x] = y.sort
 end 
+     nu_hash
+    end 
 end 
